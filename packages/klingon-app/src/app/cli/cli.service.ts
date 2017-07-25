16fb2bdc0685b1d23099ec1fb265491321397f2b
Wassim CHEGHAM <github@wassimchegham.com>
2017-07-25 23:17:11 +0200

    chore: update CliService with more API and fix the serialize method

--- packages/klingon-app/src/app/cli/cli.service.ts
+++ packages/klingon-app/src/app/cli/cli.service.ts
@@ -1,15 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CliService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient
+  ) { }
 
   serialize(values) {
     return Object.keys(values)
-      .filter( key => key !== 'app-name' || values[key] === '')
+      .filter( key => values[key] !== null && values[key] !== '' && key !== 'app-name')
       .map( key => `--${key}=${values[key]}`)
       .join(' ');
+  }
+
+  cliVersion() {
+    return this.runCommand('ng -v');
+  }
+
+  cliHelp() {
+    return this.runCommand('ng help');
+  }
+
+  runCommand(command) {
+    return this.http.post(`http://localhost:3000/cli/run`, {command});
   }
 
 }
