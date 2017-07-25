6d3f573f7662fd533154aedffb8464e1b4aeaaa1
Wassim CHEGHAM <github@wassimchegham.com>
2017-07-25 23:17:43 +0200

    chore: update the dropDown component's styles

--- packages/klingon-app/src/app/drop-down/drop-down.component.ts
+++ packages/klingon-app/src/app/drop-down/drop-down.component.ts
@@ -12,21 +12,53 @@ import {
   template: `
     <section>
       <header (click)="toggle()">
-        <ng-content select="h3"></ng-content>
+        <ng-content select=".title"></ng-content>
+        <div>
+          <ng-content *ngIf=" state === 'keyboard_arrow_down'"  select=".sub-title"></ng-content>
+        </div>
         <md-icon>{{state}}</md-icon>
       </header>
       <div #contentAreaRef class="content-area" [class.open]="state == 'keyboard_arrow_up'" >
-        <ng-content select="main"></ng-content>
+        <ng-content select=".content"></ng-content>
       </div>
     </section>
   `,
   styles: [`
-    :host { display: block; padding: 10px 0; }
-    :host header { display: flex; cursor: pointer; }
-    .content-area { height: 0; overflow: hidden; transition: height 100ms linear; }
-    .content-area.open { height: initial; }
-    ::ng-deep h3 { margin: 0; flex: 1 1 auto; }
-    ::ng-deep main { min-height: 50px; }
+    :host {
+      display: block; padding: 10px 0;
+    }
+    header {
+      display: flex; cursor: pointer;
+      margin-bottom: 30px;
+    }
+    header div {
+      flex: 1 1 auto;
+    }
+    .content-area {
+      height: 0; 
+      overflow: hidden; 
+      transition: height 100ms linear;
+    }
+    .content-area.open { 
+      height: initial;
+    }
+    ::ng-deep .title {
+      margin: 0; 
+      margin: 0; 
+      min-width: 30vw; 
+    }
+    ::ng-deep .sub-title {
+      padding: 6px;
+      margin: 0;
+      flex: 1 1 auto;
+      color: grey;
+      font-weight: 300;
+      font-size: 0.8em;
+      text-align: right;
+    }
+    ::ng-deep .content {
+      min-height: 50px;
+    }
   `]
 })
 export class DropDownComponent implements OnInit {
