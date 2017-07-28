3e96f73f343c2a7c1b1b6609272c82a1538fb8f6
Wassim CHEGHAM <github@wassimchegham.com>
2017-07-28 23:37:48 +0200

    chore: add a "test" component

---
+++ packages/klingon-app/src/app/cli/test/test.component.ts
@@ -0,0 +1,36 @@
+import { FlagsComponent } from './../flags/flags.component';
+import { CliService } from './../cli.service';
+import { TerminalService } from './../../terminal/terminal.service';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-cli-test",
+  templateUrl: "./test.component.html",
+  styleUrls: [
+    "./test.component.css",
+    "../flags/flags.component.css"
+  ]
+})
+export class CliTestComponent extends FlagsComponent implements OnInit {
+
+  constructor(
+    public term: TerminalService,
+    public cli: CliService
+  ) {
+    super();
+    this.form = this.buildForm(FlagsComponent.Flags.TEST);
+  }
+
+  ngOnInit() {}
+
+  serve() {
+    this.term.send(`ng test ${ this.cli.serialize(this.form.value)}`);
+  }
+
+  stop() {
+    this.term.stop();
+  }
+}
