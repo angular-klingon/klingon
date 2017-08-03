3b4b55343b51c4dba6d48841aa748acfce3dd385
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-03 22:06:07 +0200

    chore: add logs component

---
+++ packages/klingon-app/src/app/_shared/log/log.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Input, SimpleChange } from "@angular/core";
+
+export interface StdSimpleChange extends SimpleChange {
+  stderr: {currentValue:string};
+  stdout: {currentValue:string};
+}
+
+@Component({
+  selector: 'app-cli-log',
+  template: `
+    <!--<button md-raised-button (click)="clear()">Clear</button> <br/>-->
+    <section>
+      <code *ngFor="let logItem of logs"  [ngClass]="{ hasError: !!logItem.isError }"  [innerText]="logItem.text"></code>
+    </section>
+  `,
+  styleUrls: ['./log.component.css']
+})
+export class LogComponent implements OnInit {
+
+  @Input() stdout: string;
+  @Input() stderr: string;
+
+  logs: {text: string; isError?: boolean}[];
+
+  keepHistory: boolean;
+
+  constructor() {
+    this.logs = [];
+    this.keepHistory = true;
+  }
+
+  ngOnInit() {
+  }
+
+  clear() {
+    this.logs = [];
+  }
+
+  ngOnChanges(r: StdSimpleChange) {
+    if (r.stderr && r.stderr.currentValue) {
+      this.logs.push({
+        text: r.stderr.currentValue,
+        isError: true
+      });
+    }
+    if (r.stdout && r.stdout.currentValue) {
+      this.logs.push({
+        text: r.stdout.currentValue,
+        isError: false
+      });
+    }
+  }
+
+}
